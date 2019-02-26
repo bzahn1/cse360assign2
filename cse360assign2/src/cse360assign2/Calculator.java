c681ccb1f61f5d0d7519dc35e936d085cee9702b
Brian Zahn <bzahn1@asu.edu>
2019-02-25 21:12:34 -0700

    Commit of initial version of Calculator program.

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,61 @@
+package cse360assign2;
+
+/* Author: Brian Zahn 
+ * Class ID: 594
+ * Assignment 2
+ * This class mimics the functions of a calculator, providing add, subtract, 
+ * multiply, and divide methods, as well as for a method to get the total 
+ * from the operations, and a method which displays the history of actions
+ * performed.
+ */
+public class Calculator {
+	private int total;	// The total from the operations performed
+
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/*
+	 * The "getTotal" method returns the value of the total variable.
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/*
+	 * The "add" method adds the inputed value to the total variable. 
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/*
+	 * The "subtract" method subtracts the inputed value from the total variable.
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/*
+	 * The "multiply" method multiplies the inputed value by the total variable.
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/*
+	 * The "divide" method divides the total variable by the inputed value.
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/*
+	 * The "getHistory" method returns a string of each inputed operation
+	 * that was performed before the method was called.
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
